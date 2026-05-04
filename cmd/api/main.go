9ede39dc1cfdd782c46d027d5755daac970e6c9f
“Shaza“ <orlevicu@gmail.com>
2026-05-04 21:25:22 +0300

    graceful-shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,8 +5,13 @@ import (
 	"async-event-rest/internal/kafka/producer"
 	"async-event-rest/internal/repository/postgres"
 	repoRedis "async-event-rest/internal/repository/redis"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -40,9 +45,30 @@ func main() {
 	mux.HandleFunc("/events", h.CreateEvent)
 	mux.HandleFunc("/stats", h.GetStats)
 
-	log.Println("Starting API server..")
-
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
+
+	go func() {
+		log.Println("Starting API server..")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	<-quit
+	log.Println("Shutdown signal received..")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	log.Println("Server exiting...")
 }
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -8,11 +8,17 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/segmentio/kafka-go"
 )
 
-func worker(id int, jobs <-chan kafka.Message, db *postgres.Repository, cache *repoRedis.Repository) {
+func worker(id int, jobs <-chan kafka.Message, db *postgres.Repository, cache *repoRedis.Repository, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	for msg := range jobs {
 		var event models.Event
 		if err := json.Unmarshal(msg.Value, &event); err != nil {
@@ -20,7 +26,7 @@ func worker(id int, jobs <-chan kafka.Message, db *postgres.Repository, cache *r
 			continue
 		}
 
-		log.Printf("[Worker %d] Started processing UserID=%d, Type=%s", id, event.UserID, event.Type)
+		log.Printf("[Worker %d] Processing UserID=%d, Type=%s", id, event.UserID, event.Type)
 
 		if err := db.SaveEvent(event); err != nil {
 			log.Printf("[Worker %d] Failed to save event: %v", id, err)
@@ -36,9 +42,8 @@ func worker(id int, jobs <-chan kafka.Message, db *postgres.Repository, cache *r
 			log.Printf("[Worker %d] Failed to invalidate cache: %v", id, err)
 			continue
 		}
-
-		log.Printf("[Worker %d] Finished processing UserID=%d", id, event.UserID)
 	}
+	log.Printf("[Worker: %d] Shutted down.", id)
 }
 
 func main() {
@@ -60,21 +65,40 @@ func main() {
 	defer kafkaConsumer.Close()
 	log.Println("Connected to Kafka(consumer) successfully! Starting workers..")
 
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
 	jobs := make(chan kafka.Message, 100)
 
 	numWorkers := 5
 	for w := 1; w <= numWorkers; w++ {
-		go worker(w, jobs, db, cache)
+		wg.Add(1)
+		go worker(w, jobs, db, cache, &wg)
 	}
 
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		log.Println("Shutdown signal received..")
+		cancel()
+	}()
+
 	for {
-		msg, err := kafkaConsumer.ReadMessage(context.Background())
+		msg, err := kafkaConsumer.ReadMessage(ctx)
 		if err != nil {
+			if ctx.Err() != nil {
+				break
+			}
 			log.Printf("Error reading message: %v", err)
 			continue
 		}
 
 		jobs <- msg
-
 	}
+
+	close(jobs)
+	wg.Wait()
+
+	log.Println("All workers are shutted down. Extiting...")
 }
